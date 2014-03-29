4bc71dfd31c96310227795fac965ff68f8447de5
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-03-28 17:16:33 -0700

    Run git operations on update
    There's also a pconfigure fix here.

--- update.bash
+++ update.bash
@@ -1,16 +1,32 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+git pull
+git submodule init
+git submodule update
 
 top="$(pwd)"
 prefix="$top/install"
 
-export PATH="$prefix/bin:$PATH"
-export PKG_CONFIG_PATH="$prefix/lib/pkgconfig:$PKG_CONFIG_PATH"
+cat >"$prefix"/enter <<EOF
+export PATH="$prefix/bin:\$PATH"
+export PKG_CONFIG_PATH="$prefix/lib/pkgconfig:\$PKG_CONFIG_PATH"
+EOF
+source "$prefix"/enter
 
+echo "Building pconfigure"
 cd "$top"/src/pconfigure/
-./bootstrap.sh --prefix "$prefix"
+
+if test -f Configfiles/local
+then
+    pconfigure
+else
+    ./bootstrap.sh --prefix "$prefix"
+fi
+
 make
 make install
 
+echo "Building tek"
 cd "$top"/src/tek/
 pconfigure
 make
