c9105c319fea1252be6a5ad2edae1e69b9073893
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-03-28 17:12:09 -0700

    Add tek and build script

--- update.bash
+++ update.bash
@@ -4,8 +4,12 @@ prefix="$top/install"
 export PATH="$prefix/bin:$PATH"
 export PKG_CONFIG_PATH="$prefix/lib/pkgconfig:$PKG_CONFIG_PATH"
 
-cd src/pconfigure/
-./bootstrap.sh --prefix $prefix
+cd "$top"/src/pconfigure/
+./bootstrap.sh --prefix "$prefix"
 make
 make install
 
+cd "$top"/src/tek/
+pconfigure
+make
+make install
